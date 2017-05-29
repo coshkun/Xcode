6fd44f79ea6bc85fcef8ed97bfbc086ab169d94d
Coskun Caner <cskncnr@gmail.com>
2017-05-30 01:29:09 +0300

    update

--- fancy-box/myFirstApp/myFirstApp/ViewController.m
+++ fancy-box/myFirstApp/myFirstApp/ViewController.m
@@ -13,10 +13,26 @@
 @end
 
 @implementation ViewController
+@synthesize etiket;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //[etiket setText:@"merhaba iOS dünyası!"];
+    CGRect myFrame = CGRectMake(10, 10, 200, 50);
+    
+    UILabel *myLable = [[UILabel alloc] init];
+    //[myLable setFrame:CGRectMake(50, 50, 200, 50)];
+    [myLable setFrame:myFrame];
+    [myLable setText:@"My Lable with Code"];
+    [myLable setTextAlignment:NSTextAlignmentLeft];
+    [myLable setBackgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0]];
+    [myLable setTextColor:[UIColor whiteColor]];
+    
+    
+    [self.view addSubview:myLable];
+    
 }
 
 - (void)didReceiveMemoryWarning {
