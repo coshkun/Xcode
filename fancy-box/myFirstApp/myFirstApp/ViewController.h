ded3fa8ae8347742e10ff8f1358f6cd7dda59c2d
Coskun Caner <cskncnr@gmail.com>
2017-05-30 04:58:25 +0300

    update

--- fancy-box/myFirstApp/myFirstApp/ViewController.h
+++ fancy-box/myFirstApp/myFirstApp/ViewController.h
@@ -8,9 +8,15 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    int count;
+}
+
+-(IBAction)labelOlustur;
+-(IBAction)resmiDegistir;
 
 @property (nonatomic, retain) IBOutlet UILabel *etiket;
+@property (nonatomic, retain) IBOutlet UIImageView *myImageView;
 
 @end
 
--- fancy-box/myFirstApp/myFirstApp/ViewController.m
+++ fancy-box/myFirstApp/myFirstApp/ViewController.m
@@ -14,11 +14,13 @@
 
 @implementation ViewController
 @synthesize etiket;
+@synthesize myImageView;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    /*
     //[etiket setText:@"merhaba iOS dünyası!"];
     CGRect myFrame = CGRectMake(10, 10, 200, 50);
     
@@ -32,12 +34,39 @@
     
     
     [self.view addSubview:myLable];
+    */
+    count = 0;
     
+    [myImageView setImage:[UIImage imageNamed:@"oyuncak.png"]]; 
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)labelOlustur{
+    CGRect myFrame = CGRectMake(10, 100, 200, 50);
+    
+    UILabel *myLable = [[UILabel alloc] init];
+    
+    [myLable setFrame:myFrame];
+    [myLable setText:@"My Lable with Code"];
+    [myLable setTextAlignment:NSTextAlignmentLeft];
+    [myLable setBackgroundColor:[UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:0]];
+    [myLable setTextColor:[UIColor whiteColor]];
+    
+    [self.view addSubview:myLable];
+}
+
+-(IBAction)resmiDegistir{
+    if (count % 2 == 0) {
+        [myImageView setImage:[UIImage imageNamed:@"ucak.png"]];
+    }
+    else{
+        [myImageView setImage:[UIImage imageNamed:@"oyuncak.png"]];
+    }
+    count++;
 }
 
 @end
