3a19e8fd75dbe6d61145e6e638b0359dac6cb0f4
Coskun Caner <cskncnr@gmail.com>
2017-07-03 14:22:55 +0300

    working Beta without Polish on BullEye

--- ISMK.92225-Lectures/MyCosHomeWorks/01.BullsEye/BullsEye/BullsEye/ViewController.swift
+++ ISMK.92225-Lectures/MyCosHomeWorks/01.BullsEye/BullsEye/BullsEye/ViewController.swift
@@ -13,16 +13,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var sldrBull: UISlider!
     @IBOutlet weak var btnHitMe: UIButton!
     @IBOutlet weak var lblTarget: UILabel!
+    @IBOutlet weak var lblRound: UILabel!
+    @IBOutlet weak var lblSkore: UILabel!
     
-    var HitValue:Int = 50
+    var HitValue:Int = 0
     var TargetValue:Int = 0
+    var RoundCounter:Int = 0
+    var Skore:Int = 0
+    let SnapTolerence:Int = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         HitValue = lroundf(sldrBull.value)
         TargetValue = newTarget()
-        lblTarget.text = "Put the Bull's Eye as close as you can to: \(TargetValue)"
+        updateLabels()
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,9 +36,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnHitMe_ShowAlert(sender: UIButton) {
+        var newSP:Int = 0
+        var newTitle:String = "Not So Bad,"
+        
+        if TargetValue==HitValue {
+            newSP = TargetValue*2
+            newTitle = "Boom! x2 Multiplier for Bull's Eye"
+        }
+        else if abs(TargetValue - HitValue) <= SnapTolerence {
+            newSP = TargetValue
+            newTitle = "Nice Try!"
+        }
+        else {
+            newSP = 100 - abs(TargetValue - HitValue)
+        }
+        
         var msg:String = "The value of the slider is now: \(HitValue)"
-        let alert = UIAlertController(title: "Hello,", message: msg, preferredStyle: UIAlertControllerStyle.Alert)
-        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+            // + "\nThe target value was: \(TargetValue)"
+            + "\nYou win \(newSP) points on this round."
+        
+        let alert = UIAlertController(title: newTitle, message: msg, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { action in self.updateSkores(newSP) })
         
         alert.addAction(action)
         self.presentViewController(alert, animated: true, completion: nil)
@@ -45,7 +69,20 @@ class ViewController: UIViewController {
     }
     
     func newTarget() -> Int {
+        RoundCounter++
         return (1 + Int(arc4random_uniform(100)))
+    }
+    
+    func updateSkores(newSkorePoints:Int){
+        Skore += newSkorePoints
+        TargetValue = newTarget()
+        updateLabels()
+    }
+    
+    func updateLabels(){
+        lblTarget.text = "Put the Bull's Eye as close as you can to: \(TargetValue)"
+        lblRound.text = "Round: \(RoundCounter)"
+        lblSkore.text = "Skore: \(Skore)"
     }
 }
 
