a234fbe23d8db3433becfb44566078f20e1f0cf8
Coskun Caner <cskncnr@gmail.com>
2017-07-22 19:54:49 +0300

    update with finished BullsEye

--- ISMK.92225-Lectures/MyCosHomeWorks/01.BullsEye/BullsEye/BullsEye/AboutViewController.swift
+++ ISMK.92225-Lectures/MyCosHomeWorks/01.BullsEye/BullsEye/BullsEye/AboutViewController.swift
@@ -11,11 +11,23 @@ import UIKit
 class AboutViewController: UIViewController {
 
     @IBOutlet weak var btnAboutClose: UIButton!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var txtTitle: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if let url = NSBundle.mainBundle().URLForResource("BullsEye", withExtension: "html") {
+            if let htmlData = NSData(contentsOfFile: url.path!){
+                let baseURL = NSURL(fileURLWithPath: NSBundle.mainBundle().bundlePath)
+                webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+            }
+            
+        }
+        //Get version number
+        let nsObj: AnyObject? = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"] // CFBundleShortVersionString
+        txtTitle.text = " Bull's Eye v.\((nsObj as! String)) writen by Coskun Caner"
     }
 
     override func didReceiveMemoryWarning() {
